e42144cdef1ec1a32b01e3baf971ce383be6a301
Seungwoo-Yu <51766845+Seungwoo-Yu@users.noreply.github.com>
2021-08-11 11:02:20 +0900

    Add IOS platform

---
+++ ios/App/Unified-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Unified-Bridging-Header.h
+//  App
+//
+
+#ifndef Unified_Bridging_Header_h
+#define Unified_Bridging_Header_h
+
+#import "cordova-plugin-iosrtc-Bridging-Header.h"
+
+
+#endif /* Unified_Bridging_Header_h */
